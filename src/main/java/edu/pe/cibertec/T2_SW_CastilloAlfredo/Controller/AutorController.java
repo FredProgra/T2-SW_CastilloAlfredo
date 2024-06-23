173598031690ab41e2e8abbe9c45bb20b1901530
Alfredo <alfredoqooo@gmail.com>
2024-06-23 11:58:53 -0500

    controller hecho falta ejecutatr

---
+++ src/main/java/edu/pe/cibertec/T2_SW_CastilloAlfredo/Controller/AutorController.java
@@ -0,0 +1,38 @@
+package edu.pe.cibertec.T2_SW_CastilloAlfredo.Controller;
+
+import edu.pe.cibertec.T2_SW_CastilloAlfredo.Exception.ResourceNotFoundException;
+import edu.pe.cibertec.T2_SW_CastilloAlfredo.Model.bd.Autor;
+import edu.pe.cibertec.T2_SW_CastilloAlfredo.Service.IAutorService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/autor")
+public class AutorController {
+    private IAutorService autorService;
+    @GetMapping("")
+    public ResponseEntity<List<Autor>> autorListResponse(){
+        List<Autor> autorList=new ArrayList<>(autorService.autoreslista());
+        if (autorList.isEmpty()){return new ResponseEntity<>(HttpStatus.NO_CONTENT);}
+        return new ResponseEntity<>(autorList,HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Autor> buscarAutoridResponse(@PathVariable Integer id){
+        Autor autornuevo=autorService.buscarAutorXiD(id).orElseThrow(
+                ()-> new ResourceNotFoundException("El id del autor"+id+"no existe")
+        );
+        return new ResponseEntity<>(autornuevo,HttpStatus.OK);
+
+    }
+    @PostMapping("")
+    public ResponseEntity<Autor> agregarAutorResponse(@RequestBody Autor autor){
+        return new ResponseEntity<>(autorService.agregarAutor(autor),HttpStatus.CREATED);
+
+    }
+}
