26b70dd962e94e7ef8509f01c3442a930a162402
Alfredo <alfredoqooo@gmail.com>
2024-06-23 12:53:38 -0500

    file controller hehco

--- src/main/java/edu/pe/cibertec/T2_SW_CastilloAlfredo/Controller/FileController.java
+++ src/main/java/edu/pe/cibertec/T2_SW_CastilloAlfredo/Controller/FileController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,9 +19,34 @@ import java.util.List;
 @AllArgsConstructor
 public class FileController {
     private FileService fileService;
+    private static final List<String> ALLOWED_EXTENSIONS = Arrays.asList("pdf", "png", "docx");
     @PostMapping("")
     public ResponseEntity<ArchivoDto> subirArchivo(@RequestParam("files") List<MultipartFile> multipartFileList) throws Exception{
         fileService.GuardarArchivos(multipartFileList);
         return  new ResponseEntity<>(ArchivoDto.builder().mensaje("Archivos subidos correctamente").build(), HttpStatus.OK);
+    }
+    @PostMapping("/uploadMultiple")
+    public ResponseEntity<String> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        if (files.length != 3) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Debe subir exactamente 3 archivos.");
+        }
+
+        for (MultipartFile file : files) {
+            String fileExtension = getFileExtension(file.getOriginalFilename());
+            if (!ALLOWED_EXTENSIONS.contains(fileExtension)) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Todos los archivos deben ser pdf, png, o docx.");
+            }
+        }
+
+
+
+        return ResponseEntity.status(HttpStatus.OK).body("Archivos subidos exitosamente.");
+    }
+    private String getFileExtension(String fileName) {
+        if (fileName == null) {
+            return null;
+        }
+        String[] parts = fileName.split("\\.");
+        return parts.length > 1 ? parts[parts.length - 1] : null;
     }
 }
--- src/main/java/edu/pe/cibertec/T2_SW_CastilloAlfredo/Service/FileService.java
+++ src/main/java/edu/pe/cibertec/T2_SW_CastilloAlfredo/Service/FileService.java
@@ -1,12 +1,13 @@
 package edu.pe.cibertec.T2_SW_CastilloAlfredo.Service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
+@Service
 public class FileService implements IFileService{
     private final Path pathFolder= Paths.get("upsloads");
     @Override
